34d7417bb6360d5ddd7f0d684a1e6430822101da
wangzhihao <wangzhihao.chn@qq.com>
2021-06-24 23:09:35 +0800

    7. 反转整数

--- src/main/java/com/wzh/leetcode/solution/ReverseInteger_7.java
+++ src/main/java/com/wzh/leetcode/solution/ReverseInteger_7.java
@@ -1,7 +1,7 @@
 package com.wzh.leetcode.solution;
 
 /**
- * 7.反转整数 Reverse Integer
+ * 7. 反转整数 Reverse Integer
  * <p>
  * 题目描述：
  * 给定一个 32 位有符号整数，将整数中的数字进行反转。
@@ -23,17 +23,35 @@ package com.wzh.leetcode.solution;
  * 根据这个假设，如果反转后的整数溢出，则返回 0。
  */
 public class ReverseInteger_7 {
-    /*
+    /**
      * 算法思路：
-     *    依次获得输入数字从低位到高位的数字，进行反转。
-     *
+     * 依次获得输入数字从低位到高位的数字，进行反转。
+     * <p>
      * 注意：
-     *    1. 翻转后越界问题 —— 用long保存结果，返回前做判断
-     *    2. 符号问题 —— 逐位取余，符号不会发生变化
-     *    3. 终止条件 —— 不是tmp<0，因为有可能输入本身为负数
-     *
-     *  */
+     * 1. 翻转后越界问题 —— 由于不能使用64位整数，因此需要验证过程可以投机为：
+     * 每次计算得到的新值除以10后不等于旧值，表示越界，直接返回0
+     * 2. 符号问题 —— 逐位取余，符号不会发生变化
+     * 3. 终止条件 —— 不是tmp<0，因为有可能输入本身为负数
+     * <p>
+     * tips:为什么1对，java int计算的特性，运算结果超出了范围(Integer.MIN_VALUE~Integer.MAX_VALUE)时
+     * 则发生了溢出，而且不会有任何异常抛出，计算结果为低32位的值。
+     */
     public static int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int t = x % 10;
+            int newRes = 10 * res + t;
+            // 表示有溢出
+            if ((newRes - t) / 10 != res) {
+                return 0;
+            }
+            res = newRes;
+            x /= 10;
+        }
+        return res;
+    }
+
+    public static int reverseOldVersion(int x) {
         long res = 0;
         int tmp = x;
         while (tmp != 0) {
@@ -46,6 +64,6 @@ public class ReverseInteger_7 {
     }
 
     public static void main(String[] args) {
-        System.out.println(reverse(-123));
+        System.out.println(reverse(2123456789));
     }
 }
